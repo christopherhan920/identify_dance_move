6cc69305cd61caf0920b6a18551c126dcab40df5
Chris Ong <christopherisaac.ong@gmail.com>
2022-09-13 09:04:32 -0500

    new react app

---
+++ idm_react3/my-app/src/App.tsx
@@ -0,0 +1,72 @@
+import logo from './logo.svg';
+import React, {Component} from 'react';
+import './App.css';
+import axios from 'axios'
+
+class App extends Component{
+  // constructor(props){
+  //   super(props);
+  //   this.upload = this.upload.bind(this);
+  // }
+  state = {
+    file: null
+  }
+
+  upload(){
+    console.log('upload')
+    let file = this.state.file
+    // axios.post({
+    //   url:'http://127.0.0.1:5000/',
+    //   method: 'POST',
+    //   headers:{
+
+    //   },
+    //   data: file,
+    // }).then((res) =>{
+    //   console.log('done')
+    // })
+  }
+
+  // handleFile(e){
+  //   let file = e.target.files[0]
+  //   console.log(file)
+  //   this.setState({file:file})
+  // }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form>
+            <h1>Dance File Upload</h1>
+            {/* <input type="file" name="file" onChange={(e) => this.handleFile(e)}/> */}
+            <button type="submit" onClick={this.upload}> Upload</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a>
+//           Learn React
+//         </a>
+//         <form>
+//           <h1>Dance File Upload</h1>
+//           <input type="file" name="file" onChange={(e) => this.handleFile(e)}/>
+//           <button type="submit" onClick={upload}>Upload</button>
+//         </form>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
