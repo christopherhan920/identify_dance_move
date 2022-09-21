345dbd522343a14773342747784d416c76051250
christopherhan <14696+christopherhan@users.noreply.github.tamu.edu>
2022-09-21 12:06:33 -0500

    post method returns prediction

--- model_backend.py
+++ model_backend.py
@@ -57,56 +57,66 @@ def load_video(path: str, max_frames=0, resize=(IMG_SIZE, IMG_SIZE)):
         cap.release()
     return np.array(frames)
 
-def preprocess_video(df, root_dir):
-    num_samples = len(df)
-    video_paths = df["video_name"].values.tolist()
-    labels = df["tag"].values
-    labels = label_processor(labels[..., None]).numpy()
+def build_feature_extractor():
+    feature_extractor = keras.applications.InceptionV3(
+        weights="imagenet",
+        include_top=False,
+        pooling="avg",
+        input_shape=(IMG_SIZE, IMG_SIZE, 3),
+    )
+    preprocess_input = keras.applications.inception_v3.preprocess_input
+
+    inputs = keras.Input((IMG_SIZE, IMG_SIZE, 3))
+    preprocessed = preprocess_input(inputs)
+
+    outputs = feature_extractor(preprocessed)
+    return keras.Model(inputs, outputs, name="feature_extractor")
+
+feature_extractor = build_feature_extractor()
+
+def preprocess_video(path):
 
     # `frame_masks` and `frame_features` are what we will feed to our sequence model.
     # `frame_masks` will contain a bunch of booleans denoting if a timestep is
     # masked with padding or not.
-    frame_masks = np.zeros(shape=(num_samples, MAX_SEQ_LENGTH), dtype="bool")
+    frame_masks = np.zeros(shape=(1, MAX_SEQ_LENGTH), dtype="bool")
     frame_features = np.zeros(
-        shape=(num_samples, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32"
+        shape=(1, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32"
     )
 
-    # For each video.
-    for idx, path in enumerate(video_paths):
-        print('Working on video {}'.format(idx))
-        # Gather all its frames and add a batch dimension.
-        frames = load_video(os.path.join(root_dir, path))
-        frames = frames[None, ...]
+    # Gather all its frames and add a batch dimension.
+    frames = load_video(path)
+    frames = frames[None, ...]
 
-        # Initialize placeholders to store the masks and features of the current video.
-        temp_frame_mask = np.zeros(shape=(1, MAX_SEQ_LENGTH,), dtype="bool")
-        temp_frame_features = np.zeros(
-            shape=(1, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32"
-        )
+    # Initialize placeholders to store the masks and features of the current video.
+    temp_frame_mask = np.zeros(shape=(1, MAX_SEQ_LENGTH,), dtype="bool")
+    temp_frame_features = np.zeros(
+        shape=(1, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32"
+    )
 
-        # Extract features from the frames of the current video.
-        for i, batch in enumerate(frames):
-            video_length = batch.shape[0]
-            length = min(MAX_SEQ_LENGTH, video_length)
-            for j in range(length):
-                temp_frame_features[i, j, :] = feature_extractor.predict(
-                    batch[None, j, :]
-                )
-            temp_frame_mask[i, :length] = 1  # 1 = not masked, 0 = masked
+    # Extract features from the frames of the current video.
+    for i, batch in enumerate(frames):
+        video_length = batch.shape[0]
+        length = min(MAX_SEQ_LENGTH, video_length)
+        for j in range(length):
+            temp_frame_features[i, j, :] = feature_extractor.predict(
+                batch[None, j, :]
+            )
+        temp_frame_mask[i, :length] = 1  # 1 = not masked, 0 = masked
 
-        frame_features[idx,] = temp_frame_features.squeeze()
-        frame_masks[idx,] = temp_frame_mask.squeeze()
+    frame_features[0,] = temp_frame_features.squeeze()
+    frame_masks[0,] = temp_frame_mask.squeeze()
 
-    return (frame_features, frame_masks), labels
+    return (frame_features, frame_masks)
 
 def load_model():
     model = keras.models.load_model(model_path)
     print('loaded model: {}'.format(model_path))
     return model
 
-def predict(video: np.array) -> dict:
-    # take the input video as np.array and return prediction
-    print('predicting')
+# def predict(video: np.array) -> dict:
+#     # take the input video as np.array and return prediction
+#     print('predicting')
 
 
 @app.route('/post', methods=['POST'])
@@ -121,12 +131,18 @@ def POST():
         os.remove(temp_path)
 
     with open(temp_path, 'wb') as out_file:
-        out_file.write(request.data)
+        out_file.write(video_bytes)
     
-    video_array = load_video(temp_path)
+    video_array = preprocess_video(temp_path)
     # from the point the video is a numpy array
-    print(video_array)
-    
-    return jsonify(video_array)
+    model = load_model()
+    result = model.predict(video_array)[0]
+    # print(result)
+    result_dict = {'Stick and Roll': str(result[0]),
+                    'Brooklyn': str(result[1]),
+                    'Charleston': str(result[2]),
+                    'Monastery': str(result[3])
+                    }
+    return jsonify(result_dict)
 
 app.run(debug=True)
